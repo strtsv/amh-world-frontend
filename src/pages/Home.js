8c76e1b3616fa955e81f81435590f7248b2e1db3
jsusilo <enr@bk.ru>
2022-04-09 23:47:10 +0500

    homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Home/Header";
+import Content from "../components/Home/Content";
+import Footer from "../components/Home/Footer";
+
+const Home = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
