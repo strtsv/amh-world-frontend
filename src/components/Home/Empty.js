f4c19c81cc725b4521e7fe87008302924804474f
jsusilo <enr@bk.ru>
2022-04-16 00:02:09 +0500

    home empty comp

---
+++ src/components/Home/Empty.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Empty extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="container">
+          <div className="col-md-6"></div>
+          <div className="col-md-6" />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Empty;
